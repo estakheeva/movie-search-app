4c08e3990cd7dff24c2a136d43de38e528d0b9b7
elestaada <kl.gogen@gmail.com>
2026-08-07 08:56:19 +0300

    Extract MovieGrid component and fix ESLint config

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,7 @@
       @clear="onClear"
     />
 
-    <div v-if="movies.length" class="row">
-      <div v-for="film in movies" :key="film.filmId" class="col-6 col-sm-4 col-lg-3 col-xl-2 mb-3">
-        <div class="card bg-secondary text-white h-100">
-          <img
-            v-if="film.posterUrlPreview && !film.posterUrlPreview.includes('no-poster')"
-            :src="film.posterUrlPreview"
-            class="card-img-top"
-            :alt="film.nameRu || film.nameEn"
-          />
-          <div v-else class="card-img-top d-flex align-items-center justify-content-center bg-dark text-light" style="height: 200px;">
-            <span>Нет постера</span>
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">{{ film.nameRu || film.nameEn }}</h5>
-            <p class="card-text">{{ film.year }}</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <MovieGrid :movies="movies" />
 
     <div v-if="movies.length" class="text-center mt-4">
       <button @click="loadMore" class="btn btn-outline-light">Показать ещё</button>
@@ -36,6 +18,7 @@
 <script setup>
 import { ref } from 'vue'
 import SearchBar from './components/SearchBar.vue'
+import MovieGrid from './components/MovieGrid.vue'
 
 const movies = ref([])
 const loading = ref(false)
